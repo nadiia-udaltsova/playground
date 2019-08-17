2977d21a8da83db8c9d185d6f4614eb8eadc1d14
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-17 18:55:44 +0300

    упражнение глава 2

---
+++ projects/SimpleCSharpConsoleApp/SimpleCSharpConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Настройка консольного пользовательского интерфейса.
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine ("*******************************");
+            Console.WriteLine("***Welcome to My Rocking App******");
+            Console.WriteLine("***************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            // Ожидание нажатия клавиши <Enter>.
+            Console.ReadLine();
+            MessageBox.Show("All done!");
+        }
+    }
+}
