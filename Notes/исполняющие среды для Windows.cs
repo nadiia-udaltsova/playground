33122a168c421f635a4c12bb06304537fd2367bc
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-19 09:35:04 +0300

    исполняющие среды для Windows

---
+++ Notes/исполняющие среды для Windows.cs
@@ -0,0 +1,10 @@
+Исполняющие среды для Windows
+
+-Visual Studio Express
+-Visual Studio Community
+-Visual Studio Professional
+
+Все проекты .Net имеют доступ к фундаментальной библиотеке mscorlib.dll
+Среда Express позволяет строить программное обеспечение только под управлением ОС Windows.
+Visual Studio Community работает на разных платформах
+Mono - межплатформенная реализация .Net
