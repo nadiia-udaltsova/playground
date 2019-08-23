7875ee622a99c301a7166737270bc324c007aa00
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-23 08:39:29 +0300

    выражение if - else if

---
+++ projects/выражение if - else if/выражение if - else if/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace выражение_if___else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 33;
+            if (x ==8)
+            { Console.WriteLine("Value of x is 8");
+                Console.ReadKey();
+            }
+            else if (x==18)
+            {
+              Console.WriteLine("Value of x is 18");
+              Console.ReadKey();
+            }
+            else if (x == 33)
+            {
+                Console.WriteLine("Value of x is 33");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No match");
+                Console.ReadKey();
+            }
+            //Outputs "Value of x is 33"
+        }
+    }
+}
