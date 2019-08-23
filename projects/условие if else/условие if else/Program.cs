47caf263e69122d6dcbab7b783cba81c2ca3852b
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-23 07:58:01 +0300

    условие if else

---
+++ projects/условие if else/условие if else/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace условие_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mark = 85;
+            if(mark < 50)
+            {
+                Console.WriteLine("You failed.");
+            }
+            else
+            {
+                Console.WriteLine("You passed.");
+                Console.ReadKey();
+            }
+            //Outputs "You passed."
+        }
+    }
+}
