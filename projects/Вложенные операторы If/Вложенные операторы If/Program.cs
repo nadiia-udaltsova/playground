8aeb940355b4598bb1376cd2a8b5674fc237af44
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-23 08:12:42 +0300

    Вложенные операторы If

---
+++ projects/Вложенные операторы If/Вложенные операторы If/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вложенные_операторы_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 18;
+            if (age > 14)
+            {
+                if (age > 18)
+                {
+                    Console.WriteLine("Adult");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Teenager");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                if (age > 0)
+                {
+                    Console.WriteLine("Child");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Something`s wrong");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
