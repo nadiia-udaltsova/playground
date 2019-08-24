fd2725b2e00f2d0ce1ba6512b738422760bf8abd
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-24 18:23:34 +0300

    цикл while

---
+++ projects/цикл while/цикл while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цикл_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x++<5)
+            {
+                if (x % 2 == 0)
+                    x += 2;
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
