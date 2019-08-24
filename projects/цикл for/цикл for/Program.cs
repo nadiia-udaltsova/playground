b725e1c5c288f739e1d93b3c5ee7481d2ca46cfb
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-24 18:31:48 +0300

    цикл for

---
+++ projects/цикл for/цикл for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цикл_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            for (; x > 0;) ;
+            {
+                Console.WriteLine(x);
+                x -= 3;
+                Console.ReadKey();
+            }
+        }
+    }
+}
