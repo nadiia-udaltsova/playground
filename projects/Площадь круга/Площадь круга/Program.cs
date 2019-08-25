4a65d27753c03b3c6ffa7b6ba66f6898c94955e0
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-25 10:47:59 +0300

    площадь круга

---
+++ projects/Площадь круга/Площадь круга/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Площадь_круга
+{
+    //вычислить площадь круга.
+    class Program { }
+        class Circle
+    {
+        static void Main(string[] args)
+        { double radius;
+            double area;
+            radius = 10.0;
+            area = radius * radius * 3.1416;
+            Console.WriteLine("Площадь равна" + area);
+
+        }
+    }
+}
