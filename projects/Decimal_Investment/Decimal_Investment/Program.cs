98afb266001c0cb6daa90a4370c2989c7d054b0c
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-25 12:25:27 +0300

    тип decimal для расчета будущей стоимости капиталовложений

---
+++ projects/Decimal_Investment/Decimal_Investment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_Investment
+{
+    /*Применить тип decimal для расчета будущей стоимости 
+     * капиталовложений.
+     */
+    class FutVal
+    {
+        static void Main(string[] args)
+        {
+            decimal amount;
+            decimal rate_of_return;
+            int years, i;
+
+            amount = 1000.0M;
+            rate_of_return = 0.07M;
+            years = 10;
+
+            Console.WriteLine("Первоночальные капиталовложения:$" + amount);
+            Console.WriteLine("Норма прибыли" + rate_of_return);
+            Console.WriteLine("В течении" + years + "лет");
+
+            for (i = 0; i < years; i++)
+                amount = amount + (amount * rate_of_return);
+            Console.WriteLine("Будущая стоимость равна:$" + amount);
+            Console.ReadLine();
+        }
+    }
+}
