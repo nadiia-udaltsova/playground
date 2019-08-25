2e1162e870b994ab6035ba0cb2f05c12fc876a1a
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-25 11:51:19 +0300

    тип decimal для расчета скидки

---
+++ projects/Decimal_Price/Decimal_Price/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_Price
+{
+    class UseDecimal
+    {
+        static void Main(string[] args)
+        {
+            decimal price;
+            decimal discount;
+            decimal discounted_price;
+            // Рассчитать цену со скидкой.
+            price = 19.95m;
+            discount = 0.15m;//норма скидки составляет 15%.
+            discounted_price = price - (price * discount);
+            Console.WriteLine("Цена со скидкой:$" + discounted_price);
+            Console.ReadLine();
+        }
+    }
+}
