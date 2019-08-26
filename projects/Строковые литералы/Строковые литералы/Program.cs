6168ebba739b8abed3ce4d4a44909db42c961336
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-26 11:43:58 +0300

    Строковые литералы

---
+++ projects/Строковые литералы/Строковые литералы/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Строковые_литералы
+{
+    //Продемонстрировать применение управляющих 
+    //последовательностей символов в строковых литералах.
+    class StrDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первая строка\nВторая строка\nТретья строка");
+            Console.WriteLine("Один\tДва\tТри");
+            Console.WriteLine("Четыре\tПять\tШесть");
+
+            //Вставить кавычки.
+            Console.WriteLine("\"Зачем?\", спросил он.");
+            Console.ReadLine();
+
+        }
+    }
+}
