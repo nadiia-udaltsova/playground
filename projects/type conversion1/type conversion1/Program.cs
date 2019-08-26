3d9e0ab085d4b6ccfcf02d104765af6205e1bcbd
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-26 15:49:59 +0300

    type conversion, Integer, double

---
+++ projects/type conversion1/type conversion1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace type_conversion1
+{
+    //Пример приведения типов в выражениях.
+    class CastExpr
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            for (n = 1.0; n <= 10; n++) 
+            {
+                Console.WriteLine("Квадратный корень из{0} равен {1}", n, Math.Sqrt(n));
+                Console.WriteLine("Целая часть числа:(0)", (int) Math.Sqrt(n));
+                Console.WriteLine("Дробная часть числа:(0)", Math.Sqrt(n) - (int)Math.Sqrt(n));
+                Console.WriteLine();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
