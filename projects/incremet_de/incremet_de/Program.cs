f2aeeb0e960598c05d3b33eda0fcecf06fec0424
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-27 19:39:44 +0300

    increment example

---
+++ projects/incremet_de/incremet_de/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incremet_de
+{
+    //продемонстрировать отличие между префиксной и 
+    //постфиксной формами оператора инкремента (++)
+    class PrePostDemo
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            int i;
+
+            x = 1;
+            y = 0;
+            Console.WriteLine("Ряд чисел, полученных" + "с помощью оператора y= y+ x++");
+            for(i=0; i<10; i++)
+            { y = y + x++; //постфиксная форма оператора ++
+                Console.WriteLine(y + "");
+            }
+            Console.WriteLine();
+            x = 1;
+            y = 0;
+            Console.WriteLine("Ряд чисел, полученных" + "с помощью оператора y=y+ ++x");
+            for(i=0; i<10; i++)
+            { y = y + ++x;//префиксная форма оператора ++
+                Console.WriteLine(y + "");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
