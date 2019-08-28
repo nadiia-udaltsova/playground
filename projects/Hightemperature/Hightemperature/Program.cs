a36e692ce5ce3cddad383f5016dc3477f93b86b0
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-28 12:24:01 +0300

    how to works operator ||

---
+++ projects/Hightemperature/Hightemperature/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hightemperature
+{
+    //пример работы логического оператора "укороченное ИЛИ"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isHighTemperature = true;
+            bool hasNoCooling = true;
+            if (isHighTemperature||hasNoCooling)
+            {
+                Console.WriteLine("Угроза повреждения процессора!!!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
