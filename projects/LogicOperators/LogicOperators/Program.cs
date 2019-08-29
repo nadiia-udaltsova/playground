e744703ca86f9d12102232669132cbcf2bdad8c3
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-29 12:30:32 +0300

    Logic operators - Gerbert Shildt

---
+++ projects/LogicOperators/LogicOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicOperators
+{
+    //Продемонстрировать применение операторов
+    // отношения и логических операторов.
+    class RelLogOps
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            bool b1, b2;
+
+            i = 10;
+            j = 11;
+            if (i < j) Console.WriteLine("i<j");
+            if (i <= j) Console.WriteLine("i<=j");
+            if (i != j) Console.WriteLine("i!=j");
+
+            if (i == j) Console.WriteLine("Нельзя выполнить");
+            if (i >= j) Console.WriteLine("Нельзя выполнить");
+            if (i > j) Console.WriteLine("Нельзя выполнить");
+
+            b1 = true;
+            b2 = false;
+            if (b1 & b2) Console.WriteLine("Нельзя выполнить");
+            if (!(b1 & b2)) Console.WriteLine("!(b1&b2) - true");
+            if (b1 | b2) Console.WriteLine("b1|b2=true");
+            if (b1 ^ b2) Console.WriteLine("b1^b2 - true");
+
+            Console.ReadLine();
+
+        }
+    }
+}
