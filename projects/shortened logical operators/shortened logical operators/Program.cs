500994683c34bb18b2bcc13c0bedca3d7d0a904f
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-29 14:42:35 +0300

    shortened logical operators

---
+++ projects/shortened logical operators/shortened logical operators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shortened_logical_operators
+{
+    class SCops
+    {
+        static void Main(string[] args)
+        {
+            int n, d;
+
+            n = 10;
+            d = 2;
+            if (d != 0 && (n % d) == 0)
+                Console.WriteLine(n + "делится нацело на" + d);
+
+            d = 0; //задать нулевое значение переменной d.
+            // d равно 0, поэтому второй операнд не вычесляется
+
+            if (d != 0 && (n % d) == 0) 
+            Console.WriteLine(n + "делится нацело на" + d);
+
+            //Если теперь попытаться сделать тоже самое без укороченного
+            // логического оператора, то возникнет ошибка из-за деления на 0.
+
+            if (d != 0 & (n % d) == 0)
+                Console.WriteLine(n + "делится нацело на" + d);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ projects/shortened logical operators/shortened logical operators/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("shortened logical operators")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("shortened logical operators")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f5fcdb32-4a4c-42b9-af14-3ff20917f8f5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
