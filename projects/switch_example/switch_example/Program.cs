400c76affd803a31c28f461ab1dc6e4c5851da6f
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-30 16:52:47 +0300

    применение оператора switch

---
+++ projects/switch_example/switch_example/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_example
+{
+    //продемонстрировать применение оператора switch.
+
+    class SwitchDemo
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 0; i < 10; i++) 
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("i равно нулю");
+                        break;
+                    case 1:
+                        Console.WriteLine("i равно единице");
+                        break;
+                    case 2:
+                        Console.WriteLine("i равно двум");
+                        break;
+                    case 3:
+                        Console.WriteLine("i равно трем");
+                        break;
+                    case 4:
+                        Console.WriteLine("i равно 4");
+                        break;
+                    default:
+                        Console.WriteLine("i равно или больше пяти");
+                        Console.ReadLine();
+                        break;
+                       
+
+                }
+        }
+    }
+}
