62d34e05c09791d13d9d8877864cbc85abb85940
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-30 15:54:48 +0300

    В этой программе обнаруживается наименьший множитель заданного целого значения, состоящий из одной цифры.

---
+++ projects/if_else if_1/if_else if_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_if_1
+{
+    class Ladder
+    {
+        //Определить наименьший множитель заданного
+        //целого значения, состоящий из одной цифры.
+        static void Main(string[] args)
+        {
+            int num;
+
+            for (num = 2; num < 12; num++)
+            {
+                if ((num % 2) == 0)
+            Console.WriteLine("Наименьший множитель числа " + num + "равен 2. ");
+            else if ((num % 3) == 0)
+                Console.WriteLine("Наименьший множитель числа " + num + "равен 3. ");
+            else if ((num % 5) == 0)
+                Console.WriteLine("Наименьший множитель числа " + num + "равен 5. ");
+            else if ((num % 7) == 0)
+                Console.WriteLine("Наименьший множитель числа " + num + "равен 7. ");
+            else
+                Console.WriteLine(num + " не делится на 2,3,5 или 7. ");
+                Console.ReadLine();
+             }
+        
+        }
+    }
+    
+}
