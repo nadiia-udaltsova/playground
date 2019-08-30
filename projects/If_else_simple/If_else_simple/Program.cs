1cdbdb83d637828cda5ed81c8435769c4a4ec5e2
nadiia-udaltsova <recruterchoice@gmail.com>
2019-08-30 14:06:57 +0300

    if else - simple example

---
+++ projects/If_else_simple/If_else_simple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_else_simple
+{
+    class PosNeg
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = -5; i <= 5; i++)
+            {
+                Console.WriteLine("Проверка:" + i + "");
+                if (i < 0) Console.WriteLine("отрицательное число");
+                else Console.WriteLine("положительное число");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
