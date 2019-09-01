ede87d1ed1f42627a8866419eebb49d63e2abfd4
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-01 13:39:07 +0300

    практика 1

---
+++ projects/practice1/ClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    private class Class1
+    {
+    }
+}
---
+++ projects/practice1/practice1/Program.cs
@@ -0,0 +1,130 @@
+﻿
+
+
+namespace Moto
+{
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+
+        public void Run()
+        { 
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Break()
+        {
+
+        }
+    }
+
+    public static class EifelTower
+    {
+        public static void Shine()
+        {
+
+        }
+    }
+
+    public class Engine
+    {
+
+    }
+
+    public class Driver
+    {
+
+    }
+}
+
+namespace practice1
+{
+    using System;
+    using Moto;
+    using Things;
+    public static class Programm
+    {
+        public static void Main(string[] args)
+        {
+            var jeans = new Cloth() {
+                Size = 30, Type = "jeans", Mark = "Adidass", 
+            };
+            var dress = new Cloth()
+            {
+                Size = 48, Type = "dress", Mark = "Gucci",
+            };
+
+            jeans.Wash();
+            jeans.Shew();
+            dress.Wash();
+            dress.Shew();
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace Things
+{
+    using System;
+    public class Cloth
+    {
+        public int Size { get; set; }
+        public string Type { get; set; }
+        public string Mark { get; set; }
+        public void Wash()
+        {
+            string readableSize = Size < 40 ? "small" : "big";
+            /*string readableSize = "";
+
+            if (Size < 40)
+            {
+                readableSize = "small";
+            }
+            else
+            {
+                readableSize = "big";
+            }*/
+
+            string message = "I Wash " + Type + " of size " + Size + " And it is a " + readableSize + " item";
+            Console.WriteLine(message);
+        }
+
+        /*
+             I'm sewing jeans of size 30 with mark Adidass
+             I'm sewing dress of size 48 with mark Gucci
+        */
+  
+        
+        
+       
+
+    }
+}
+
+namespace Things
+{
+    using System;
+    public class Cloth
+    {
+        public int Size { get; set; }
+        public string Type { get; set; }
+        public string Mark { get; set; }
+        public void Shew ()
+        {
+            Console.WriteLine("I`m sewing " + Type + " of size" + Size + " with mark " + Mark);
+        }
+    }
+}
+
+        
+
+
