c5e4ae0f2380ddd7112ebe99189c096a47f04f4d
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-03 15:51:06 +0300

    Объявление управляющих переменных в цикле for

---
+++ ForVar/ForVar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForVar
+{
+    //Обьявить переменную правления циклом в самом цикле for.
+    class ForVar
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int fact = 1;
+            //вычислить фактириал чисел от 1 до 5
+            for (int i = 1; i <= 5; i++)
+            {
+                sum += i; //переменная i действует в цикле.
+                fact *= i;
+            }
+            // а здесь переменная i недоступна.
+            Console.WriteLine("Сумма равна " + sum);
+            Console.WriteLine("Факториал равен " + fact);
+        }
+    }
+}
