070aa50b00dda6a9297659d804aafab413f3908c
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-03 16:17:02 +0300

    Применить оператор break для выхода из цикла

---
+++ BreakDemo/BreakDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakDemo
+{
+    //Применить оператор Break для выхода из цикла.
+    class BreakDemo
+    {
+        static void Main(string[] args)
+        {
+            //Использовать оператор break для выхода из этого цикла.
+            for (int i = -10; i <= 10; i++)
+            {
+                if (i > 0) break; // завершить цикл, как только значение переменной i станет положительным.
+                Console.WriteLine (i + " ");
+            }
+            Console.WriteLine("Готово!");
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ BreakDemo/BreakDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("BreakDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BreakDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3482db3a-b528-4ec9-8351-1e6b5b783b6f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
