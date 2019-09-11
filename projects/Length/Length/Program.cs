20ef65e7dbf3ac9490175a865806201dbd69c4c0
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-11 14:56:31 +0300

    свойство Length массива

---
+++ projects/Length/Length/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Использовать свойство Length массива.
+
+namespace Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            Console.WriteLine("Длина массива nums равна " + nums.Length);
+            // Использовать свойство Length для инициализации массива nums.
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = i * i;
+            // А теперь воспользоваться свойством Length
+            // для вывода содержимого массива nums.
+            Console.Write("Содержимое массива nums: ");
+            for (int i = 0; i < nums.Length; i++)
+                Console.Write(nums[i] + " ");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
