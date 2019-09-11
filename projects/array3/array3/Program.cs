cb440ed8253a9627609e623ba5c0591a2bee342d
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-11 14:34:40 +0300

    двумерный массив

---
+++ projects/array3/array3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Инициализировать двумерный массив.
+namespace array3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] sqrs = 
+            {
+                { 1, 1 } ,
+                { 2, 4 } ,
+                { 3, 9} ,
+                { 4, 16} ,
+                { 5, 25} ,
+                { 6, 36} ,
+                { 7, 49} ,
+                { 8, 64} ,
+                { 9, 81} ,
+                { 10, 100} ,
+
+            };
+            int i, j;
+
+            for(i = 0;  i<10; i++ )
+            {
+                for (j = 0; j < 2; j++)
+                    Console.Write(sqrs[i, j] + " ");
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
