81b652b75d61d143ce85187fd6aa9569a63a5a62
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-11 14:13:29 +0300

    многомерный массив

---
+++ projects/array2/array2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Суммировать значения по одной из диагоналей матрицы 3×3×3.
+
+namespace array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] m = new int[3, 3, 3];
+            int sum = 0;
+            int n = 1;
+
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++)
+                    for (int z = 0; z < 3; z++)
+                        m[x, y, z] = n++;
+            sum = m[0, 0, 0] + m[1, 1, 1] + m[2, 2, 2];
+
+            Console.WriteLine("Сумма значений по первой диагонали: " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
