3e29d032b40ff5c33817c290eeb88fba47262fa1
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-13 13:35:48 +0300

    метод Substring()

---
+++ projects/Substring/Substring/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Применить метод Substring().
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string orgstr = "В C# упрощается обращение со строками.";
+
+            // сформировать подстроку
+            string substr = orgstr.Substring(5, 20);
+
+            Console.WriteLine("orgstr: " + orgstr);
+            Console.WriteLine("substr: " + substr);
+            Console.ReadLine();
+        }
+    }
+}
