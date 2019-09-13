2b921729ecbd4e45d8a31f8f252b8e32a6889709
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-13 13:53:25 +0300

    управление оператором switch посредством строк

---
+++ projects/string_switch/string_switch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Продемонстрировать управление оператором switch посредством строк.
+
+namespace string_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "один", "два", "три", "два", "один" };
+
+            foreach (string s in strs)
+            {
+                switch (s)
+                {
+                    case "один":
+                        Console.Write(1);
+                        Console.ReadLine();
+                        break;
+                    case "два":
+                        Console.Write(2);
+                        Console.ReadLine();
+                        break;
+                    case "три":
+                        Console.Write(3);
+                        Console.ReadLine();
+                        break;
+
+                }
+            }
+        }
+    }
+}
