be9e18074437d2455fff9be273616dd5d7ddcd6c
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-14 14:41:16 +0300

    вывести символьную строку

---
+++ projects/Stroka/Stroka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stroka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charray = { 'Э', 'т', 'о', ' ', 'с', 'т', 'р', 'о', 'к', 'а' };
+
+            string str1 = new string(charray);
+            string str2 = "Еще одна строка.";
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.ReadLine();
+
+        }
+    }
+}
