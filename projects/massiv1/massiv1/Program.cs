b7988a4d307645c13a5b38fd1805b1342ceb612a
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-16 13:05:03 +0300

    массивы

---
+++ projects/massiv1/massiv1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 10;
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = i;
+            }
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("'Элемент массива с индексом " + i + " имеет значение " + array[i]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
