fd832fec5379482a31c39b0287f5be89d09e36c6
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-17 16:12:01 +0300

    дз, вызов метода

---
+++ projects/home17.09/home17.09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home17._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sentenceFairytale = { "Красная шапочка", "Снежная Королева", "Колобок" };
+            Write(sentenceFairytale);
+            Console.ReadLine ();
+        }
+
+        private static void Write (string [] sentence)
+        {
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                Console.WriteLine(sentence[i]);
+            }
+        }
+    }
+}
