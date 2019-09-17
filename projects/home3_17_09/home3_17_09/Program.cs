96cf9a8f5c3e1fc89026f16fbc9f9f7d7f645308
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-17 16:42:03 +0300

    home3_17_09

---
+++ projects/home3_17_09/home3_17_09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home3_17_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 30, 67, 89, 76, 3, 6, 98, 12 };
+            int y = 345;
+            string car = "Ford";
+            PrintandPrint(x, y, car);
+            Console.ReadLine();
+
+        }
+
+        static void PrintandPrint(int[] x, int y, string car)
+        {
+            for (int i = 0; i < x.Length; i++)
+                Console.WriteLine(x[i], y, car);
+        }
+            
+    
+
+    }
+}
