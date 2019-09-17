70196bde4488f8ba5ea3ef5b97f2b3b929f58c0a
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-17 16:21:48 +0300

    дз. вызов метода2

--- projects/home2_17_09/home2_17_09/Program.cs
+++ projects/home2_17_09/home2_17_09/Program.cs
@@ -10,6 +10,18 @@ namespace home2_17_09
     {
         static void Main(string[] args)
         {
+            int[] money = { 1, 2, 5, 10, 25, 50 };
+            Count(money);
+            Console.ReadLine();
+
+        }
+
+        private static void Count (int [] coins)
+        {
+            for (int i = 0; i < coins.Length; i++) 
+            {
+                Console.WriteLine (coins[i]);
+            }
         }
     }
 }
