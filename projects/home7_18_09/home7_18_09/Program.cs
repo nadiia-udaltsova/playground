d667e1099baef201386f39a275c1380754985fec
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-18 19:43:37 +0300

    ДЗ.Вызов метода

---
+++ projects/home7_18_09/home7_18_09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home7_18_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] students = { 34, 67, 89, 90, 112, 157 };
+            int age = 20;
+            string nationality = "ukrainian";
+            Teach(students, age, nationality);
+            Console.ReadLine();
+        }
+        static void Teach(int[] students, int age, string nationality)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+                Console.WriteLine(age);
+                Console.WriteLine(nationality);
+            }
+        }
+    }
+}
