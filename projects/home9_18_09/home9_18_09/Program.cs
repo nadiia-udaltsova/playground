88af382087848dbd11204dc44b3aab4be630678c
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-18 20:02:30 +0300

    Дз.Вызов метода

---
+++ projects/home9_18_09/home9_18_09/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home9_18_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] building = { "apartment", "cottage", "country house" };
+            int storeys = 5;
+            int amount = 8;
+            string developer = "Renessans";
+            Build(building, storeys, amount, developer);
+            Console.ReadLine();
+
+        }
+        static void Build(string[] building, int storeys, int amount, string developer)
+        {
+            for (int i = 0; i < building.Length; i++)
+            {
+                Console.WriteLine(building [i]);
+                Console.WriteLine(storeys);
+                Console.WriteLine(amount);
+                Console.WriteLine(developer);
+            }
+        }
+    }
+}
