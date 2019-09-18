11cf70afd16983d85861d736a6e507c10ec260cf
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-18 16:34:38 +0300

    Дз.Вызов метода.

---
+++ projects/home4_18_09/home4_18_09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home4_18_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] books = { "Garry Pother", "Never Stop", "Shepot volka", "Uzdniki gorizonta" };
+            int numers = 2005;
+            int shelf = 14;
+            string library = "Makeevka";
+            Readbooks(books, numers, shelf, library);
+            Console.ReadLine();
+        }
+        static void Readbooks (string[] books, int numers, int shelf, string library)
+        {
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine(books[i]);
+                Console.WriteLine(numers);
+                Console.WriteLine(shelf);
+                Console.WriteLine(library);
+            }
+        }
+    }
+}
