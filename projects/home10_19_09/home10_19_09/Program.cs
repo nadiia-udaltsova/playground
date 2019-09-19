096fa11552f6bfa35f14b0c7d11943d8ae6ee855
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-19 13:39:57 +0300

    дз

---
+++ projects/home10_19_09/home10_19_09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home10_19_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] things = { "dishes", "saucepan", "pan" };
+            int amount = 5;
+            string day = "Saturday";
+            Wash(things, amount, day);
+            Console.ReadLine();
+
+        }
+        static void Wash (string [] things, int amount, string day)
+        {
+            for (int i = 0; i < things.Length; i++)
+            {
+                Console.WriteLine(things [i]);
+                Console.WriteLine(amount);
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
