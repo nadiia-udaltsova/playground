625dcc493daf33eb6ca586d3715823c7aa54d7e2
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-20 14:33:34 +0300

    foreach

---
+++ projects/foreach1/foreach1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Поиск в массиве с помощью оператора цикла foreach.
+
+namespace foreach1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            int[] nums = new int[10];
+            int val;
+            bool found = false;
+
+            // Задать первоначальные значения элементов массива nums.
+
+            for (int i = 0; i < 10; i++)
+                nums[i] = i;
+
+            val = 5;
+
+            // Использовать цикл foreach для поиска заданного
+            // значения в массиве nums.
+
+            foreach (int x in nums)
+            {
+                if (x == val)
+                found = true;
+                break;
+            }
+            if (found) 
+            Console.WriteLine("Значение найдено!");
+        }
+             
+            
+    }
+}
