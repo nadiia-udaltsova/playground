cd2ac4fc29ec614fd9cf8e9f55336bfd2fdac5d9
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-25 15:27:10 +0300

    необязательные аргументы

---
+++ projects/optional arguments/optional arguments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Продемонстрировать необязательные аргументы.
+
+namespace optional_arguments
+{
+    class OptionArgDemo
+    {
+        static void OptArgMeth (int alpha, int beta = 10, int gamma = 20)
+        {
+            Console.WriteLine("Это аргументы alpha, beta и gamma: " + alpha + " " + beta
+                               + " " + gamma);
+        }
+        static void Main ()
+        {
+            // Передать все аргументы явным образом.
+            OptArgMeth(1, 2, 3);
+
+            // Сделать аргумент gamma необязательным.
+            OptArgMeth(1, 2);
+
+            // Сделать оба аргумента beta и gamma необязательными.
+            OptArgMeth(1);
+
+            Console.ReadLine();
+
+        }
+    }
+}
