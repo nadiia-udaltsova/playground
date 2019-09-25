3bfee1ba22203ec9bfe0393183c6a9a555c7d9d5
nadiia-udaltsova <recruterchoice@gmail.com>
2019-09-25 10:50:13 +0300

    Инициализаторы объектов

---
+++ projects/Object Initializers/Object Initializers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Простой пример, демонстрирующий применение инициализаторов объектов.
+
+namespace Object_Initializers
+{
+    class MyClass
+    {
+        public int Count;
+        public string Str;
+    }
+    class ObjInitDemo
+    {
+        static void Main()
+        {
+            // Сконструировать объект типа MyClass, используя инициализаторы объектов.
+
+            MyClass obj = new MyClass { Count = 100, Str = "Тестирование" };
+            Console.WriteLine(obj.Count + " " + obj.Str);
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
