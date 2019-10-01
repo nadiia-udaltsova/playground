05f1b212395306d8097c1dc48d00dafd8b60d5ba
nadiia-udaltsova <recruterchoice@gmail.com>
2019-10-01 18:19:15 +0300

    оператор switch

---
+++ projects/Prometeus_switch/Prometeus_switch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometeus_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            string s;
+            Console.WriteLine("Задайте значение 1,2 или 3");
+            number = Console.ReadLine();
+
+            switch(number)
+            {
+                case "1": s = "прогулка"; break;
+                case "2": s = "спорт"; break;
+                case "3": s = "програмирование"; break;
+                default: s = "не корректное введение данных"; break;
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
