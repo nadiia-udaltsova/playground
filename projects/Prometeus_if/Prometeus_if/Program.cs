d4d89db87103ada0d5bc170c4047ab4c12e11815
nadiia-udaltsova <recruterchoice@gmail.com>
2019-10-01 18:29:36 +0300

    оператор if, else if

---
+++ projects/Prometeus_if/Prometeus_if/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometeus_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number;
+            
+            Console.WriteLine("Задайте число 1,2 или 3");
+            number = Console.ReadLine();
+            if (number == "1")
+            {
+                string s = "спорт";
+                Console.WriteLine(s);
+            }
+            else if (number == "2")
+            {
+                Console.WriteLine("прогулка");
+
+            }
+            else if (number == "3")
+            {
+                Console.WriteLine("програмирование");
+            }
+            else
+            {
+                Console.WriteLine("неккоректное введение");
+            }
+            Console.ReadLine();
+             
+        }
+    }
+}
