844076c04cab48aac9341f788de67dd595fe73d2
nadiia-udaltsova <recruterchoice@gmail.com>
2019-10-24 14:53:52 +0300

    примеры

--- JavaScript/newsite/javascript.js
+++ JavaScript/newsite/javascript.js
@@ -1,1 +1,27 @@
+let user = {
+    name: "John",
+    age: 30,
+    profeccion: "driver",
+    cars: ["toyota", "ford", "mazda", "bmw"],
+    sayHi: function (){
+        console.log("Hello from Kyiv! My name is " + this.name)
+    }
 
+};
+let arrsCars = user.cars.join(','); //String.join(",", user.cars); 
+console.log(arrsCars);
+for (let i=0; i<user.cars.length; i++){
+    console.log(user.cars[i]);
+}
+user.name = "Roma";
+user.sayHi();
+console.log(user.age);
+
+
+function getTitle(){
+    return "This is title";
+}
+
+let xxx = getTitle();
+
+console.log(getTitle())
