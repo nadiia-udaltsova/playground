f92dff6583ef2ba44bdd5f4e60cc341dfa244b5c
nadiia-udaltsova <recruterchoice@gmail.com>
2019-10-31 17:12:20 +0300

    no message

--- JavaScript/Website 1/javascript.js
+++ JavaScript/Website 1/javascript.js
@@ -1,4 +1,4 @@
-let a = 0;
+/*let a = 0;
 let b = false;
 if (a===b){
     console.log("a === b");
@@ -30,4 +30,80 @@ function readNumber() {
     return +number;
   }
   
-  alert(`Число: ${readNumber()}`);
+  alert(`Число: ${readNumber()}`);*/
+
+  var subject = "Just a string";
+  var probe = typeof subject;
+  console.log (probe);
+
+  var test1 = "abcdf";
+  var test2 = 123;
+  var test3 = true;
+  var test4 = {};
+  var test5 = [];
+  var test6;
+  var test7 = {"abcdef": 123};
+  var test8 = ["abcdef", 123];
+  function test9(){return "abcdef"};
+
+console.log(typeof test1);
+console.log(typeof test2);
+console.log(typeof test3);
+console.log(typeof test4);
+console.log(typeof test5);
+console.log(typeof test6);
+console.log(typeof test7);
+console.log(typeof test8);
+console.log(typeof test9);
+
+var test10 = null;
+console.log(typeof test10);
+
+if (99 == "99") {
+  console.log("A number equals a string!");
+  } else {
+  console.log("No way a number equals a string");
+  }
+
+
+
+  function findCarInLot(car) {
+    for (var i = 0; i<lot.length; i++) {
+      if (car == lot[i]) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  var chevy = {
+    make: "Chevy",
+    model: "Bel Air"
+    };
+    var taxi = {
+    make: "Webville Motors",
+    model: "Taxi"
+    };
+    var fiat1 = {
+    make: "Fiat",
+    model: "500"
+    };
+    var fiat2 = {
+    make: "Fiat",
+    model: "500"
+    };
+    var lot = [chevy, taxi, fiat1, fiat2];
+
+    debugger;
+    console.log(findCarInLot(chevy));
+    console.log(findCarInLot(taxi));
+    console.log(findCarInLot(fiat1));
+    console.log(findCarInLot(fiat2));
+
+
+var input = "jenny@wickedlysmart.com";
+for(var i = 0; i < input.length; i++) {
+if (input.charAt(i) === "@") {
+console.log("There's an @ sign at index " + i);
+}
+
+}
