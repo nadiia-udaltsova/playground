be6d4cf8568afb5b4d67f2f06fff739b32fed8f8
nadiia-udaltsova <recruterchoice@gmail.com>
2019-10-31 17:34:42 +0300

    no message

--- JavaScript/newsite/javascript.js
+++ JavaScript/newsite/javascript.js
@@ -1,1 +1,17 @@
-console.log("ddd");
+var phrase = "the cat in the hat";
+var index = phrase.indexOf("cat");
+console.log("there's a cat sitting at index " + index);
+index = phrase.indexOf("the", 5);
+console.log("there's a the sitting at index " + index);
+index = phrase.indexOf("dog");
+console.log("there's a dog sitting at index " + index);
+
+var data = "name|phone|address";
+var val = data.substring(5,10);
+console.log("Substring is " + val);
+val = data.substring(5);
+console.log("Substring is " + val);
+
+var data1 = "name|phone|address";
+var vals = data.split("|");
+console.log("Split array is " , vals);
