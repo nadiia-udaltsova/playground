c9cde83654b966963eb8aedcb553a3255629ffc4
nadiia-udaltsova <recruterchoice@gmail.com>
2019-11-03 19:38:43 +0300

    no message

--- JavaScript/exercises 2.11/javascript.js
+++ JavaScript/exercises 2.11/javascript.js
@@ -8,3 +8,12 @@ function counter(element){
   counter1++;
   element.innerHTML = "На этот текст было наведено " + counter1 + " раз";
 }
+
+function printTime(){
+  var d = new Date();
+  var hours = d.getHours();
+  var mins = d.getMinutes();
+  var secs = d.getSeconds();
+  document.body.innerHTML = hours + ":" + mins +":"+ secs;
+}
+setInterval(printTime, 1000);
