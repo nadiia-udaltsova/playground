838b3dc8f6e25426dd4a92b6cecc7e89a136645c
nadiia-udaltsova <recruterchoice@gmail.com>
2019-11-07 15:06:15 +0300

    no message

--- JavaScript/website2/javascript.js
+++ JavaScript/website2/javascript.js
@@ -1,1 +1,16 @@
 console.log("Hello word!");
+
+
+let guestList = "Guests:\n * John\n * Pete\n * Mary";
+alert(guestList);
+
+/*Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
+
+ucFirst("вася") == "Вася";*/
+function ucFirst(str) {
+    if (!str) return str;
+  
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  
+  alert( ucFirst("вася") );
