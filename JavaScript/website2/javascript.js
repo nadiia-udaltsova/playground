a0224cc9c346d7414c9847d99ec7515ba4241b89
nadiia-udaltsova <recruterchoice@gmail.com>
2019-11-09 14:34:17 +0300

    no message

--- JavaScript/website2/javascript.js
+++ JavaScript/website2/javascript.js
@@ -7,10 +7,29 @@ alert(guestList);
 /*Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом. Например:
 
 ucFirst("вася") == "Вася";*/
-function ucFirst(str) {
-    if (!str) return str;
-  
-    return str[0].toUpperCase() + str.slice(1);
-  }
-  
-  alert( ucFirst("вася") );
+
+//ucFirst("вася") =="Вася";
+function ucFirst(str){
+  if(!str) return str;
+  return str[0].toUpperCase() + str.slice(1);
+
+}
+console.log(ucFirst("вася"));
+
+function checkSpam(newstr){
+  let lowerStr = newstr.toLowerCase();
+  return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+}
+console.log(checkSpam('buy ViAgRA now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam("innocent rabbit"));
+
+function truncate(str, maxlength) {
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
+}
+
+function cutStr(happy, 4) {
+  return (happy.length > 4) ?
+  happy.slice(0, 4-1) + '…' : happy;
+}
